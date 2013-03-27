b102466127a0c691aae4d04eaf1fe4c231dee330
Rodrigo dos Santos <rodrigosamaster@gmail.com>
2013-03-27 16:42:33 -0300

    added file and class comments

--- app/code/community/Rosantoz/Moip/Block/Payment/Child.php
+++ app/code/community/Rosantoz/Moip/Block/Payment/Child.php
@@ -1,7 +1,34 @@
 <?php
 
+/**
+ * Child block
+ *
+ * PHP version 5.3.8
+ *
+ * @category Pet_Projects
+ * @package  Rosantoz_Moip
+ * @author   Rodrigo dos Santos <falecom@rodrigodossantos.ws>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     https://rodrigodossantos.ws
+ */
+
+/**
+ * Child block to show a payment link on user's account
+ *
+ * @category Pet_Projects
+ * @package  Rosantoz_Moip
+ * @author   Rodrigo dos Santos <falecom@rodrigodossantos.ws>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  Release: <1.0>
+ * @link     https://rodrigodossantos.ws
+ */
 class Rosantoz_Moip_Block_Payment_Child extends Mage_Core_Block_Template
 {
+    /**
+     * Shows a payment link on user's account
+     * 
+     * @return void
+     */
     public function _construct()
     {
         $request = $this->getRequest();
--- app/code/community/Rosantoz/Moip/Block/Payment/Info.php
+++ app/code/community/Rosantoz/Moip/Block/Payment/Info.php
@@ -1,13 +1,42 @@
 <?php
 
+/**
+ * Info block
+ *
+ * PHP version 5.3.8
+ *
+ * @category Pet_Projects
+ * @package  Rosantoz_Moip
+ * @author   Rodrigo dos Santos <falecom@rodrigodossantos.ws>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     https://rodrigodossantos.ws
+ */
+
+/**
+ * Info block to add a child block to Mage_Payment_Block_Info
+ *
+ * @category Pet_Projects
+ * @package  Rosantoz_Moip
+ * @author   Rodrigo dos Santos <falecom@rodrigodossantos.ws>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  Release: <1.0>
+ * @link     https://rodrigodossantos.ws
+ */
+
 class Rosantoz_Moip_Block_Payment_Info extends Mage_Payment_Block_Info
 {
+    /**
+     * Creates a child block
+     * 
+     * @return void
+     */
     protected function _construct()
     {
         parent::_construct();
-        $this->setChild('rosantoz_moip_repay', new Rosantoz_Moip_Block_Payment_Child());
-
-        //var_dump($this->getInfo());
+        $this->setChild(
+            'rosantoz_moip_repay', 
+            new Rosantoz_Moip_Block_Payment_Child()
+        );
     }
 
 }
